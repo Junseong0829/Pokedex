d010e16ebdd5cda66f26408035ac1327842ddf06
Junseong Lee <kirara8029@naver.com>
2025-07-30 00:36:45 +0900

    test commit

--- src/routeTree.gen.ts
+++ src/routeTree.gen.ts
@@ -12,12 +12,19 @@
 
 import { Route as rootRoute } from './routes/__root'
 import { Route as IndexImport } from './routes/index'
+import { Route as PagesPokemonImport } from './routes/pages/pokemon'
 
 // Create/Update Routes
 
 const IndexRoute = IndexImport.update({
   id: '/',
   path: '/',
+  getParentRoute: () => rootRoute,
+} as any)
+
+const PagesPokemonRoute = PagesPokemonImport.update({
+  id: '/pages/pokemon',
+  path: '/pages/pokemon',
   getParentRoute: () => rootRoute,
 } as any)
 
@@ -32,6 +39,13 @@ declare module '@tanstack/react-router' {
       preLoaderRoute: typeof IndexImport
       parentRoute: typeof rootRoute
     }
+    '/pages/pokemon': {
+      id: '/pages/pokemon'
+      path: '/pages/pokemon'
+      fullPath: '/pages/pokemon'
+      preLoaderRoute: typeof PagesPokemonImport
+      parentRoute: typeof rootRoute
+    }
   }
 }
 
@@ -39,32 +53,37 @@ declare module '@tanstack/react-router' {
 
 export interface FileRoutesByFullPath {
   '/': typeof IndexRoute
+  '/pages/pokemon': typeof PagesPokemonRoute
 }
 
 export interface FileRoutesByTo {
   '/': typeof IndexRoute
+  '/pages/pokemon': typeof PagesPokemonRoute
 }
 
 export interface FileRoutesById {
   __root__: typeof rootRoute
   '/': typeof IndexRoute
+  '/pages/pokemon': typeof PagesPokemonRoute
 }
 
 export interface FileRouteTypes {
   fileRoutesByFullPath: FileRoutesByFullPath
-  fullPaths: '/'
+  fullPaths: '/' | '/pages/pokemon'
   fileRoutesByTo: FileRoutesByTo
-  to: '/'
-  id: '__root__' | '/'
+  to: '/' | '/pages/pokemon'
+  id: '__root__' | '/' | '/pages/pokemon'
   fileRoutesById: FileRoutesById
 }
 
 export interface RootRouteChildren {
   IndexRoute: typeof IndexRoute
+  PagesPokemonRoute: typeof PagesPokemonRoute
 }
 
 const rootRouteChildren: RootRouteChildren = {
   IndexRoute: IndexRoute,
+  PagesPokemonRoute: PagesPokemonRoute,
 }
 
 export const routeTree = rootRoute
@@ -77,11 +96,15 @@ export const routeTree = rootRoute
     "__root__": {
       "filePath": "__root.tsx",
       "children": [
-        "/"
+        "/",
+        "/pages/pokemon"
       ]
     },
     "/": {
       "filePath": "index.tsx"
+    },
+    "/pages/pokemon": {
+      "filePath": "pages/pokemon.tsx"
     }
   }
 }
